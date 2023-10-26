eb4ba6f53bb5e26f5caddefa5308f5d4d0588e7a
BELMAKSENE Souhaib <148861917+souhaibmk22@users.noreply.github.com>
2023-10-26 08:53:06 +0200

    ok

--- script.js
+++ script.js
@@ -4,16 +4,3 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
-
-  document.addEventListener("DOMContentLoaded", function () {
-    const modeToggle = document.getElementById("mode-toggle");
-    const stylesheet = document.getElementById("stylesheet");
-
-    modeToggle.addEventListener("click", function () {
-        if (stylesheet.getAttribute("href") === "style.css") {
-            stylesheet.setAttribute("href", "night-mode.css");
-        } else {
-            stylesheet.setAttribute("href", "style.css");
-        }
-    });
-});
